74b67933a97d704e88ee3bd8596ff84af64e605c
fiyaznkp <fiyaznkp@gmail.com>
2023-12-16 17:17:15 +0530

    body parser in express

--- bodyparser.js
+++ bodyparser.js
@@ -4,3 +4,18 @@ const app = express()
 
 const port = 5173
 
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.urlencoded({ extended: false }))
+
+app.use(bodyParser.json())
+
+app.get("/",(req,res)=>{
+    res.send("home page")
+})
+
+app.post("/p",(req,res)=>{
+    res.send(req.body)
+})
+
+app.listen(port)
